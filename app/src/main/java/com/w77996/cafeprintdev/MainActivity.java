a4b22f815830c589bf689a66a0c3e7e3db91a908
w77996 <912513404@qq.com>
2018-01-10 22:01:36 +0800

    服务器编写

--- app/src/main/java/com/w77996/cafeprintdev/MainActivity.java
+++ app/src/main/java/com/w77996/cafeprintdev/MainActivity.java
@@ -2,54 +2,113 @@ package com.w77996.cafeprintdev;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
 
-import com.yanzhenjie.andserver.AndServer;
-import com.yanzhenjie.andserver.Server;
+import com.w77996.cafeprintdev.broadcast.ServerManager;
+
+import java.util.LinkedList;
+
 
 public class MainActivity extends AppCompatActivity {
-    Server mServer;
+    private ServerManager mServerManager;
+    private TextView mTvMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AndServer andServer = new AndServer.Build()
-                .listener(mListener)
-                .port(8080) // 默认是8080，Android平台允许的端口号都可以。
-                .timeout(10 * 1000) // 默认10 * 1000毫秒。
-                .build();
 
-// 创建服务器。
-         mServer = andServer.createServer();
+        // AndServer run in the service.
+        mServerManager = new ServerManager(this);
+        mServerManager.register();
 
-// 启动服务器。
-        mServer.start();
-
+        // startServer;
+       // findViewById(R.id.btn_start).performClick();
 
 
     }
-    private Server.Listener mListener = new Server.Listener() {
-        @Override
-        public void onStarted() {
-            // 服务器启动成功.
-        }
-
-        @Override
-        public void onStopped() {
-            // 服务器停止了，一般是开发者调用server.stop()才会停止。
-        }
-
-        @Override
-        public void onError(Exception e) {
-            // 服务器启动发生错误，一般是端口被占用。
-        }
-    };
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // 停止服务器。
-        mServer.stop();
-
-// 服务器正在运行吗？
-        boolean running = mServer.isRunning();
+        mServerManager.unRegister();
     }
+
+  /*  @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_start: {
+                showDialog();
+                mServerManager.startService();
+                break;
+            }
+            case R.id.btn_stop: {
+                mServerManager.stopService();
+                break;
+            }
+            case R.id.btn_browse: {
+                if (..... != null) {
+                    String address = mAddressList.get(1);
+                    Intent intent = new Intent();
+                    intent.setAction("android.intent.action.VIEW");
+                    Uri content_url = Uri.parse(address);
+                    intent.setData(content_url);
+                    startActivity(intent);
+                }
+                break;
+            }
+        }
+    }*/
+
+    /**
+     * Start notify.
+     */
+    public void serverStart(String ip) {
+       // closeDialog();
+        if (!TextUtils.isEmpty(ip)) {
+            //mAddressList = new LinkedList<>();
+         /*   mAddressList.add(getString(R.string.server_start_succeed));
+            mAddressList.add("http://" + ip + ":8080/");
+            mAddressList.add("http://" + ip + ":8080/login.html");
+            mAddressList.add("http://" + ip + ":8080/image");
+            mAddressList.add("http://" + ip + ":8080/download");
+            mAddressList.add("http://" + ip + ":8080/upload");*/
+        }
+        //mTvMessage.setText(TextUtils.join(",\n", mAddressList));
+    }
+
+    /**
+     * Error notify.
+     */
+    public void serverError(String message) {
+       // closeDialog();
+        mTvMessage.setText(message);
+    }
+
+    /**
+     * Started notify.
+     */
+    public void serverHasStarted(String ip) {
+        serverStart(ip);
+    }
+
+    /**
+     * Stop notify.
+     */
+    public void serverStop() {
+       // closeDialog();
+        //mAddressList = null;
+       // mTvMessage.setText(R.string.server_stop_succeed);
+    }
+/*
+    private void showDialog() {
+        if (mDialog == null)
+            mDialog = new LoadingDialog(this);
+        if (!mDialog.isShowing()) mDialog.show();
+    }
+
+    private void closeDialog() {
+        if (mDialog != null && mDialog.isShowing()) mDialog.dismiss();
+    }*/
+
 }
